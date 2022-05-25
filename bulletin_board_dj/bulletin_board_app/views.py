a2848a65179f1f7417e45c14497719add23892e0
Dmitriy Dedunik <demon19842005@yandex.ru>
2022-05-25 14:08:44 +0300

    Добавление первой страницы

--- bulletin_board_dj/bulletin_board_app/views.py
+++ bulletin_board_dj/bulletin_board_app/views.py
@@ -1,3 +1,9 @@
+from importlib.resources import contents
+from multiprocessing import context
+from re import template
 from django.shortcuts import render
 
-# Create your views here.
+def hello(request):
+    template = 'bulletin_board_app/index.html'
+    context = {}
+    return render(request, template, context)
--- bulletin_board_dj/bulletin_board_dj/settings.py
+++ bulletin_board_dj/bulletin_board_dj/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'bulletin_board_app.apps.BulletinBoardAppConfig',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +55,7 @@ ROOT_URLCONF = 'bulletin_board_dj.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [

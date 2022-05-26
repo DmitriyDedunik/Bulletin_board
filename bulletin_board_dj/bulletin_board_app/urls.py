99ae5a6050c9f2d2f230a5b41ccd3d683a08296e
Dmitriy Dedunik <demon19842005@yandex.ru>
2022-05-26 18:13:35 +0300

    добавление класса посты

--- bulletin_board_dj/bulletin_board_app/urls.py
+++ bulletin_board_dj/bulletin_board_app/urls.py
@@ -4,5 +4,6 @@ from . import views
 app_name = 'test_app'
 urlpatterns = [
     path('hello/', views.hello, name='hello'),
-    path('', views.hello, name='hello')
+    path('', views.hello, name='hello'),
+    path('posts', views.posts_list, name='posts'),
     ]
--- bulletin_board_dj/bulletin_board_app/views.py
+++ bulletin_board_dj/bulletin_board_app/views.py
@@ -3,6 +3,7 @@ from importlib.resources import contents
 from multiprocessing import context
 from re import template
 from django.shortcuts import render
+from .models import Post
 
 def hello(request):
     template = 'bulletin_board_app/index.html'
@@ -12,3 +13,12 @@ def hello(request):
         'numbers': [1,2,3,4],
     }
     return render(request, template, context)
+
+def posts_list(request):
+    template = 'bulletin_board_app/posts_list.html'
+    posts = Post.objects.all()
+    context = {
+        'posts': posts,
+    }
+    return render(request, template, context)
+

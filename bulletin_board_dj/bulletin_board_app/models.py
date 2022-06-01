cf045bc840ee4fd74940ec262332d4cde7f220d3
Dmitriy Dedunik <demon19842005@yandex.ru>
2022-06-01 14:17:10 +0300

    добавил groups_list

--- bulletin_board_dj/bulletin_board_app/models.py
+++ bulletin_board_dj/bulletin_board_app/models.py
@@ -6,7 +6,7 @@ class Post(models.Model):
     text = models.TextField(verbose_name='Текст поста')
     create = models.DateTimeField(verbose_name='Время создания', auto_now_add=True)
     group = models.ForeignKey("Group", verbose_name="Группа", on_delete=models.SET_NULL, 
-                                null=True, blank=True)
+                                null=True, blank=True, related_name='posts')
 
     class Meta:
         verbose_name = 'Публикация'
--- bulletin_board_dj/bulletin_board_app/urls.py
+++ bulletin_board_dj/bulletin_board_app/urls.py
@@ -7,4 +7,6 @@ urlpatterns = [
     path('', views.hello, name='hello'),
     path('posts/', views.posts_list, name='posts'),
     path('post/<int:post_id>/', views.post_detail, name='post'),
+    # path('group/<int:group_id>/', views.group, name='group'),
+    path('groups/', views.groups_list, name='groups'),
     ]
